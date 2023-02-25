23e03c2a3793579eb721538f7c2dee75826498d8
ball712004 <nguyenminhtam7124@gmail.com>
2023-02-24 22:08:50 -0500

    Test React

---
+++ my_app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <head> 
+        <title>My Portfolio</title>
+      </head>
+      <body>
+        <h1>My Portfolio</h1>
+        <p>My name is <strong>John Doe</strong> and I am a <em>web developer</em>.</p>
+        <p>My skills include:</p>
+      </body>  
+    </div>
+  );
+}
+
+export default App;
