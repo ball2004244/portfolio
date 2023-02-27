4dc2d7d413cf516f3b33be986f1460c3c8b3ad36
ball712004 <nguyenminhtam7124@gmail.com>
2023-02-27 12:52:25 -0500

    finish landing page

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -48,31 +48,78 @@ function Experience() {
   return ( 
     <div>
       <h1>Experience</h1>
-      <div class="experience-box">
-        <div class="experience"> 
-          <h2 class="work-position">Research Assistant</h2>
-          <h3 class="work-location">@USF</h3> 
-          <p class="job-detail">Doing Research</p>
+      <div class="experience-boxes">
+        <div class="experience-box"> 
+          <div class="front-job-detail">
+            <h2 class="work-position">Research Assistant</h2>
+            <h3 class="work-location">@USF</h3> 
+          </div>
+          <div class="job-detail">
+            <p>Doing Research</p>
+          </div>
         </div>
-        <div class="experience">
-          <h2 class="work-position">Programming Instructor</h2>
-          <h3 class="work-location">@BrightCHAMPS</h3>
-          <p class="job-detail">Teaching kids how to code</p>
+        <div class="experience-box">
+          <div class="front-job-detail">
+            <h2 class="work-position">Programming Instructor</h2>
+            <h3 class="work-location">@BrightCHAMPS</h3>
+          </div>
+          <div class="job-detail">
+            <p>Teaching kids how to code</p>
+          </div>
         </div>
-        <div class="experience">
-          <h2 class="work-position">Technical Leader</h2>
-          <h3 class="work-location">@USFGDSC</h3>
-          <p class="job-detail">Eboard member of GDSC</p>
+        <div class="experience-box">
+          <div class="front-job-detail">
+            <h2 class="work-position">Technical Leader</h2>
+            <h3 class="work-location">@USFGDSC</h3>
+          </div>
+          <div class="job-detail">
+            <p>Eboard member of GDSC</p>
+          </div>
         </div>
-        <div class="experience">
-          <h2 class="work-position">Software Engineer Intern</h2>
-          <h3 class="work-location">@KIPEVietnam</h3>
-          <p class="job-detail">Working on a project</p>
+        <div class="experience-box">
+          <div class="front-job-detail">
+            <h2 class="work-position">Software Engineer Intern</h2>
+            <h3 class="work-location">@KIPEVietnam</h3>
+          </div>
+          <div class="job-detail">
+            <p>Working on a project</p>
+          </div>
         </div>
       </div>
     </div>
   )
 }
+
+function Skill() {
+  return (
+    <div>
+      <h1>Skill</h1>
+      <div class="skill-boxes">
+        <div class="skill-box">
+          <h2>Programming Language: C++, Java, Python, JavaScript</h2>
+          <h2>Back-end: ExpressJS, Flask, Django, Postman</h2>
+          <h2>Server: NodeJS, Apache, Socket, HTTP Server</h2>
+          <h2>Database: MySQL, PostgreSQL, SQLite, MongoDB, Firebase</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <div class="contact-boxes">
+        <div class="contact-box">
+          <h2>Phone: 813-647-3229</h2>
+          <h2>Email: nguyenminhtam7124@gmail.com</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 // create a footer component
 function Footer() {
   return (
@@ -87,6 +134,8 @@ function App() {
     <div className="App">
       <Header />
       <Experience />
+      <Skill />
+      <Contact />
       <Footer />
     </div>
   );
